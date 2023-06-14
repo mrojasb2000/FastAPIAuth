dfe84a3f2aafd77f4c54d8c7f3e644a246d17623
maalroja <maalroja@bancolombia.com.co>
2023-06-13 22:11:26 -0400

    chore: add authorization login path

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ def index():
 @app.get("/users")
 def users(token: str = Depends(oauth2_scheme)):
     return "User list"
+
+
+@app.post("/token")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    return form_data.username
